56772d9a20685b0b8559fc27e7cdaef2d75df0c8
Kabbo <161012310+0xkabbo@users.noreply.github.com>
2026-02-17 14:51:08 +0600

    Create WrappedToken.sol

---
+++ WrappedToken.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+/**
+ * @title WrappedToken
+ * @dev The minted asset on the destination chain.
+ */
+contract WrappedToken is ERC20, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+
+    function mint(address to, uint256 amount) external onlyRole(MINTER_ROLE) {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) external onlyRole(MINTER_ROLE) {
+        _burn(from, amount);
+    }
+}
